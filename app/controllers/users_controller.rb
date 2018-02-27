39a145d6edfabafedc44c7e4562bd677395c0a87
hungnv950 <hungnv950@gmail.com>
2018-02-27 17:34:31 +0700

    login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update, :destroy]
+  before_action :load_user, only: [:show, :index]
 
   def index; end
 
@@ -9,6 +10,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new user_params
+    @user.avatar = "user.png"
     if @user.save
       UserMailer.account_activation(@user).deliver_now
       flash[:success] = t "account.check_email"
@@ -19,6 +21,12 @@ class UsersController < ApplicationController
   end
 
   private
+
+  def load_user
+    return if @user = User.find_by(id: params[:id])
+    flash[:danger] = t "user.not_found"
+    redirect_to root_url
+  end
 
   def user_params
     params.require(:user).permit :name, :email, :password,
